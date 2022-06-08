d01f798ef735ac6f699e66b3178b61c8dc73b689
flappy-codes <bat@flappy.codes>
2022-06-08 19:55:11 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RCC Assistance Bot'
+#copyright = ''
+author = 'flappycodes'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1'
 
 # -- General configuration
 
